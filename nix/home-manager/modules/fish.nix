f3778449891a7fe14a61010f4fe9c14aa9f4924d
ervan <ervanroot@gmail.com>
2026-03-26 19:18:51 +0700

    fix(fish): make custom PATH entries user-agnostic using /Users/ss

--- nix/home-manager/modules/fish.nix
+++ nix/home-manager/modules/fish.nix
@@ -72,37 +72,40 @@
         };
 
         interactiveShellInit = ''
-                  set -g fish_color_autosuggestion '555'  'brblack'
-                  set -g fish_color_cancel -r
-                  set -g fish_color_command --bold
-                  set -g fish_color_comment red
-                  set -g fish_color_cwd green
-                  set -g fish_color_cwd_root red
-                  set -g fish_color_end brmagenta
-                  set -g fish_color_error brred
-                  set -g fish_color_escape 'bryellow'  '--bold'
-                  set -g fish_color_history_current --bold
-                  set -g fish_color_host normal
-                  set -g fish_color_match --background=brblue
-                  set -g fish_color_normal normal
-                  set -g fish_color_operator bryellow
-                  set -g fish_color_param cyan
-                  set -g fish_color_quote yellow
-                  set -g fish_color_redirection brblue
-                  set -g fish_color_search_match 'bryellow'  '--background=brblack'
-                  set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
-                  set -g fish_color_user brgreen
-                  set -g fish_color_valid_path --underline
+          set -g fish_color_autosuggestion '555'  'brblack'
+          set -g fish_color_cancel -r
+          set -g fish_color_command --bold
+          set -g fish_color_comment red
+          set -g fish_color_cwd green
+          set -g fish_color_cwd_root red
+          set -g fish_color_end brmagenta
+          set -g fish_color_error brred
+          set -g fish_color_escape 'bryellow'  '--bold'
+          set -g fish_color_history_current --bold
+          set -g fish_color_host normal
+          set -g fish_color_match --background=brblue
+          set -g fish_color_normal normal
+          set -g fish_color_operator bryellow
+          set -g fish_color_param cyan
+          set -g fish_color_quote yellow
+          set -g fish_color_redirection brblue
+          set -g fish_color_search_match 'bryellow'  '--background=brblack'
+          set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+          set -g fish_color_user brgreen
+          set -g fish_color_valid_path --underline
 
-                  set -x CODESTATS_API_KEY $(cat ${config.sops.secrets.codestats_api_key.path})
-                  set -x GITHUB_TOKEN $(cat ${config.sops.secrets.github_token.path})
+          set -x CODESTATS_API_KEY $(cat ${config.sops.secrets.codestats_api_key.path})
+          set -x GITHUB_TOKEN $(cat ${config.sops.secrets.github_token.path})
 
-                  set -g fish_greeting ""
+          set -g fish_greeting ""
 
-                  # Initialize Starship
-                  # starship init fish | source
-          	direnv hook fish | source
-        '';
+          # custom paths to PATH (user-agnostic)
+          set -gx PATH $PATH $HOME/.local/bin/claude $HOME/.opencode/bin
+
+          # Initialize Starship
+          # starship init fish | source
+          direnv hook fish | source
+                '';
       }
 
       {
