c5f8b4908486da745b23914e9c58ee35343892f2
ervan <ervanroot@gmail.com>
2026-03-26 19:19:58 +0700

    refactor(packages): modularize PHP and Composer setup for better reusability

--- nix/home-manager/modules/packages.nix
+++ nix/home-manager/modules/packages.nix
@@ -12,29 +12,35 @@
   };
 
   config = lib.mkIf config.modules.packages.enable {
-    home.packages = with pkgs; [
-      # ruff
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.zed-mono
+    home.packages =
+      with pkgs;
+      let
+        phpWithExtensions = php82.withExtensions ({ enabled, all }: enabled ++ [ all.mongodb ]);
+        composerWithPhp = php82Packages.composer.override { php = phpWithExtensions; };
+      in
+      [
+        # ruff
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.zed-mono
 
-      tree
-      bat
+        tree
+        bat
 
-      # fastfetch
-      inputs.nixvim.packages.${pkgs.system}.default
-      pkgs.r-auth
+        # fastfetch
+        inputs.nixvim.packages.${pkgs.system}.default
+        pkgs.r-auth
 
-      nodejs_20
-      typescript
+        nodejs_20
+        typescript
 
-      (php82.withExtensions ({ enabled, all }: enabled ++ [ all.mongodb ]))
-      php82Packages.composer
+        phpWithExtensions
+        composerWithPhp
 
-      pnpm
-      # rustc
-      # cargo
-      # rustup
+        pnpm
+        # rustc
+        # cargo
+        # rustup
 
-    ];
+      ];
   };
 }
